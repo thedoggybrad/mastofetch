2097a3c4571f0ce9783ba35fb9589ade70c28941
TheDoggyBrad Software Labs <94173621+thedoggybrad@users.noreply.github.com>
2025-05-26 17:00:15 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -450,8 +450,7 @@ hr {
     max-width: 700px;
 }
 
-a[href="#"],
-a[href="PENDING"] {
+.refresh, .about {
     color: #ffffff;
     text-decoration: none;
     font-family: 'Segoe UI', Roboto, sans-serif;
@@ -470,8 +469,8 @@ a[href="PENDING"] {
 <body>
     <img class="mastogetlogo" width="128px" height="128px" src="https://res.cloudinary.com/dceum4nes/image/upload/f_auto,q_auto/v1/mastofetch/Logo_wgac9d">
     <p class="popup" style="color:white; text-align:center">
-        <a href="#" onclick="location.reload(); return false;">Refresh Content</a><br>
-        <a href="PENDING">About Mastofetch</a>
+        <a class="refresh" href="#" onclick="location.reload(); return false;">Refresh Content</a><br>
+        <a class="about" href="https://github.com/thedoggybrad/mastofetch_project/blob/main/README.md">About Mastofetch</a>
     </p><br>
     <div id="posts">
         <?php
