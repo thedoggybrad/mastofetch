6f09b1795aac718537b0f20d5aeed177839f059f
TheDoggyBrad Software Labs <94173621+thedoggybrad@users.noreply.github.com>
2025-05-27 13:26:56 +0800

    Bug fix

--- api/index.php
+++ api/index.php
@@ -321,7 +321,8 @@ if (isset($_GET['ajax']) && $_GET['ajax'] === '1') {
         $authorProfileImage = getAuthorProfileImage($post['account']);
         echo "<div class='post'>";
         echo "<img class='author-img' src='{$authorProfileImage}' alt='Author Profile Image'>";
-        echo "<p style='margin-bottom: 2px;'><strong>{$post['account']['display_name']}</strong><p class='post-time' style='margin-top: 0px;'>Posted&nbsp;" . getTimeElapsedString($post['created_at']) . "</p>";
+        echo "<p style='margin-bottom: 2px;'><strong>{$post['account']['display_name']}</strong></p>";
+        echo "<p class='post-time' style='margin-top: 0px;'>Posted&nbsp;" . getTimeElapsedString($post['created_at']) . "</p>";
         echo "<p>" . decodeEntities($post['content']) . "</p>";
 
         if (!empty($post['media_attachments'])) {
